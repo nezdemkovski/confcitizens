6249e9c661a3ae6dd784f1ddf03b3265db7f4430
Yuri Yakovlev <hi@mynameisyuri.com>
2018-12-10 14:06:50 +0100

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const { PHASE_PRODUCTION_SERVER } =
   process.env.NODE_ENV === 'development'
     ? {}
+    : !process.env.NOW_REGION
+    ? require('next/constants')
     : require('next-server/constants');
 
 // fix: prevents error when .css files are required by node
@@ -8,7 +10,7 @@ if (typeof require !== 'undefined') {
   require.extensions['.css'] = file => {};
 }
 
-module.exports = module.exports = (phase, { defaultConfig }) => {
+module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_PRODUCTION_SERVER) {
     // Config used to run in production.
     return {};
