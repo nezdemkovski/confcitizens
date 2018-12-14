77a98873169fc1e11735e9380d71bbca7f227253
Mrož <jan@mrozilla.cz>
2018-12-14 15:30:13 +0100

    Update speakers.ts
    Updated some of my information, cheers and good luck with the project!

--- data/speakers.ts
+++ data/speakers.ts
@@ -831,7 +831,7 @@ const speakers: Speaker[] = [
   },
   {
     objectID: 'mrozilla',
-    fullName: 'Jan Hruby',
+    fullName: 'Jan Hrubý',
     tags: [
       'React',
       'Redux',
@@ -849,9 +849,9 @@ const speakers: Speaker[] = [
       linkedin: 'https://www.linkedin.com/in/mrozilla/',
     },
     talks: [],
-    talkLanguages: [],
-    availableForHire: null,
-    readyToTravelAtOwnExpense: null,
+    talkLanguages: ['English', 'Czech', 'Slovak'],
+    availableForHire: true,
+    readyToTravelAtOwnExpense: false,
     currentLocation: {
       continent: 'Europe',
       country: 'Austria',
